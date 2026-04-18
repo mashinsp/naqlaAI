0a7677d8a55fa332b0ac8c2fa1896f923274dd6c
mashinsp <syedmashood334@gmail.com>
2026-04-19 02:30:33 +0500

    chore: infra

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -45,7 +45,7 @@ resource "aws_route" "public_internet" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
+  count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
